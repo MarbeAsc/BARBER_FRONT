6cff92f9d884fda072515b7a4dad66142a15146f
MarbeAsc <marbellaasencio@gmail.com>
2026-04-22 13:39:22 -0600

    Refactor:Implementación de pwa

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { registerServiceWorker } from './lib/registerServiceWorker.ts'
 import { AppProviders } from './providers/AppProviders.tsx'
+
+registerServiceWorker()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
